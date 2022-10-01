eaea4d82490deaef137b9e3d4e8c856d034a31e9
Husna Fatima <hfatima4@my.centennialcollege.ca>
2022-09-30 20:35:18 -0400

    completed controller, router, views using express

---
+++ server.js
@@ -0,0 +1,41 @@
+// Third-Party Modules
+
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import session from 'express-session';
+
+// ES Modules
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// Import Routes
+import indexRouter from './app/routes/index.route.server.js';
+
+// Instantiate Express Application
+const app = express();
+
+// Set Up Middleswares
+
+// Setup ViewEngine EJS
+app.set('views', path.join(__dirname, '/app/views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,'/client')));
+app.use(session({
+    secret: 'MySecret',
+    saveUninitialized: false,
+    resave: false
+}));
+
+//Use Routes
+
+
+app.use('/', indexRouter);
+
+app.listen(3000);
