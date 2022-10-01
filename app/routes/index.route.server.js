2c0ff1d5cf07c1911feaad09b85f86f117786a6f
Husna Fatima <hfatima4@my.centennialcollege.ca>
2022-10-01 00:01:17 -0400

    Added ejs variable and static files handler

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,10 +1,10 @@
 import { Router } from "express"; 
 
-import { goodbyeWorld, helloWorld } from "../controllers/index.controller.server.js"; 
+import { displayHomePage } from "../controllers/index.controller.server.js"; 
 
 const router = Router(); 
 
-router.get('/hello', helloWorld); 
-router.get('/goodbye', goodbyeWorld); 
+router.get('/', displayHomePage); 
+
 
 export default router;
