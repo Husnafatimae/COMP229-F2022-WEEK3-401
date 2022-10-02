e0d0459c1cad990c31f22a02e7ef35afb1355c60
Husna Fatima <hfatima4@my.centennialcollege.ca>
2022-10-02 06:10:31 -0400

    Routers and Controllers updated

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,10 +1,18 @@
 import { Router } from "express"; 
 
-import { displayHomePage } from "../controllers/index.controller.server.js"; 
+import { displayHomePage, displayAboutPage, displayProjectsPage, displayServicesPage, displayContactPage } from "../controllers/index.controller.server.js"; 
 
 const router = Router(); 
 
 router.get('/', displayHomePage); 
+router.get('/home', displayHomePage); 
+router.get('/about', displayAboutPage); 
+router.get('/projects', displayProjectsPage); 
+router.get('/services', displayServicesPage); 
+router.get('/contact', displayContactPage); 
+
+
+
 
 
 export default router;
