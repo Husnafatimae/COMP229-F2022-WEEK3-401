a1a217ea18680e11902d67e8d35cd77aab4528ca
Husna Fatima <hfatima4@my.centennialcollege.ca>
2022-10-02 07:20:20 -0400

    content added

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,18 +1,17 @@
-import { Router } from "express"; 
+import { Router } from "express";
+import { displayAboutPage, 
+    displayContactPage, 
+    displayHomePage, 
+    displayProjectsPage, 
+    displayServicesPage } from "../controllers/index.controller.server.js";
 
-import { displayHomePage, displayAboutPage, displayProjectsPage, displayServicesPage, displayContactPage } from "../controllers/index.controller.server.js"; 
+const router = Router();
 
-const router = Router(); 
-
-router.get('/', displayHomePage); 
-router.get('/home', displayHomePage); 
-router.get('/about', displayAboutPage); 
-router.get('/projects', displayProjectsPage); 
-router.get('/services', displayServicesPage); 
-router.get('/contact', displayContactPage); 
-
-
-
-
+router.get('/', displayHomePage);
+router.get('/home', displayHomePage);
+router.get('/about', displayAboutPage);
+router.get('/projects', displayProjectsPage);
+router.get('/services', displayServicesPage);
+router.get('/contact', displayContactPage);
 
 export default router;
